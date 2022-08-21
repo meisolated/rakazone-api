1e0e5418bfe6ab075c0cb3c0f6cead874cd3a415
meisolated <isolatedbot@gmail.com>
2022-08-21 20:33:38 +0530

    d

--- src/routes/userdata.js
+++ src/routes/userdata.js
@@ -1,14 +1,15 @@
 import checkLogged from "../helper/checkLogged.js"
-import { formatResponseSuccess, formatResponseError } from "../helper/index.js"
+import { formatResponseError, formatResponseSuccess } from "../helper/index.js"
 import { Users } from "../models/Users.model.js"
 
 export default function (app, path) {
- app.get(path, checkLogged, async (req, res) => {
-  try {
-   let user = await Users.findOne({ where: { user_id: req.user } }).then((user) => user.dataValues)
-   formatResponseSuccess(res, { user })
-  } catch (err) {
-   formatResponseError(res, err)
-  }
- })
+    app.get(path, checkLogged, async (req, res) => {
+        if (!req.user) return formatResponseError(res, { message: "You are not logged in", code: 401 })
+        try {
+            let user = await Users.findOne({ where: { user_id: req.user } }).then((user) => user.dataValues)
+            formatResponseSuccess(res, { user })
+        } catch (err) {
+            formatResponseError(res, err)
+        }
+    })
 }
--- src/routes/watchhistory.js
+++ src/routes/watchhistory.js
@@ -1,22 +1,20 @@
 import { formatResponseError, formatResponseSuccess } from "../helper/index.js"
-import { Sessions } from "../models/Sessions.model.js"
 import { Users } from "../models/Users.model.js"
 import { Videos } from "../models/Videos.model.js"
 import { WatchHistory } from "../models/WatchHistory.model.js"
 export default function (app, path) {
     app.post(path, async (req, res) => {
-        const { sessionId, videoId } = req.body
-        if (!sessionId || !videoId) return formatResponseError(res, { message: "Missing fields", status: 400 })
+        const { videoId } = req.body
+        if (!req.user) return formatResponseError(res, { message: "You are not logged in", code: 401 })
+        if (!videoId) return formatResponseError(res, { message: "Missing fields", status: 400 })
         try {
             const video = await Videos.findOne({ where: { videoId } })
-            const session = await Sessions.findOne({ where: { session_id: sessionId } })
-            if (!session) return formatResponseError(res, { message: "Session not found", status: 404 })
-            const data = JSON.parse(session.data)
-            const user = await Users.findOne({ where: { user_id: data.passport.user } })
+            const user = await Users.findOne({ where: { user_id: req.user } })
             if (!video || !user) return formatResponseError(res, { message: "Invalid Request", status: 400 })
-
-            const watchHistory = await WatchHistory.create({ user_id: user.id, video_id: video.id, created_at: Date.now(), modified_at: Date.now() })
-            return formatResponseSuccess(res, { watchHistory })
+            const findHistory = await WatchHistory.findOne({ where: { user_id: user.id, video_id: video.id } })
+            if (findHistory) return formatResponseError(res, { message: "You have already watched this video", status: 400 })
+            await WatchHistory.create({ user_id: user.id, video_id: video.id, created_at: Date.now(), modified_at: Date.now() })
+            return formatResponseSuccess(res, {})
         } catch (err) {
             console.log(err)
             formatResponseError(res, err)
