840eeff0cd3e99c7819587b7b6d9be02ce23e533
meisolated <isolatedbot@gmail.com>
2022-09-05 19:55:16 +0530

    updated compression level

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -4,28 +4,28 @@ import { Strategy } from "passport-google-oauth2"
 import { Users } from "../models/Users.model.js"
 
 const config = {
- clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
- clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
- callbackURL: process.env.GOOGLE_AUTH_CALLBACK_URL,
+    clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_AUTH_CALLBACK_URL,
 }
 
 let verifyCallback = (accessToken, refreshToken, profile, done) => {
- let user = { email: profile.email, name: profile.displayName, picture: profile.photos[0].value, id: profile.id }
- done(null, user)
+    let user = { email: profile.email, name: profile.displayName, picture: profile.photos[0].value, id: profile.id }
+    done(null, user)
 }
 
 passport.use(new Strategy(config, verifyCallback))
 
 passport.serializeUser(async (user, done) => {
- let _user = await Users.findOne({ where: { user_id: user.id } })
- if (_user == null) {
-  await Users.create({ user_id: user.id, name: user.name, email: user.email, profile_pic: user.picture, login_type: "google", created_on: Date.now(), last_login: Date.now() })
- } else {
-  _user.last_login = Date.now()
-  await _user.save()
- }
- done(null, user.id)
+    let _user = await Users.findOne({ where: { user_id: user.id } })
+    if (_user == null) {
+        await Users.create({ user_id: user.id, name: user.name, email: user.email, profile_pic: user.picture, login_type: "google", created_on: Date.now(), last_login: Date.now() })
+    } else {
+        _user.last_login = Date.now()
+        await _user.save()
+    }
+    done(null, user.id)
 })
 passport.deserializeUser(async (id, done) => {
- done(null, id)
+    done(null, id)
 })
