9408bd309768d4ad0b240789df9440f67990a9f7
meisolated <isolatedbot@gmail.com>
2022-09-14 10:24:04 +0530

    videoData route working

--- src/routes/userData.ts
+++ src/routes/userData.ts
@@ -4,7 +4,7 @@ export default function (app: any, path: any) {
     console.log("Loaded route: " + path)
     app.get(path, async (req: Request, res: Response) => {
         const findUser = await Users.find({ userId: req.user })
-        if (findUser) res.send({ user: findUser })
+        if (findUser) return res.send({ user: findUser })
         return res.send({ message: "User not logged In" })
     })
 }
--- src/routes/videoData.ts
+++ src/routes/videoData.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from "express"
+import { Videos } from "../models"
 export default function (app: any, path: any) {
     console.log("Loaded route: " + path)
     app.get(path, async (req: Request, res: Response) => {
-        return res.send({})
+        if (!req.body.videoId) return res.send({ message: "Invalid Request", code: 400 })
+        const findVideo = await Videos.find({ videoId: req.body.videoId })
+        if (findVideo) return res.send({ video: findVideo })
+        return res.send({ message: "Video not found", code: 404 })
     })
 }
