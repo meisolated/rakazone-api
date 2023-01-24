f2aad61634c0ec6bfb807caa9aaeef7509cf800c
prxtik69 <pratikdeshpande007pvd@gmail.com>
2023-01-24 23:31:48 +0530

    validating email in both emails and contact forms

--- src/routes/contactForm.ts
+++ src/routes/contactForm.ts
@@ -1,14 +1,18 @@
 import { Contact } from "../models"
 import { _contact } from "../models/contact.model"
 import { Request, Response } from "express"
+import { check, validationResult } from "express-validator"
 
 export default function (app: any, path: any) {
     console.log("Loaded route: " + path)
     app.post(path, async (req: Request, res: Response) => {
         const { email, name, phone, subject, message }: _contact = req.body
-        if (!email) return res.send({ message: "Invalid Request" })
-        const timestamp = Date.now()
-        Contact.create({
+        await check("email", "Please enter a valid email").isEmail().run(req)
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        const contact = new Contact({
             email,
             name,
             phone,
@@ -16,13 +20,8 @@ export default function (app: any, path: any) {
             message,
             status: "unread",
             reply: "no reply",
-            timestamp: timestamp,
         })
-            .then(() => {})
-            .catch((e) => {
-                console.log(e)
-            })
-
-        return res.json({})
+        await contact.save()
+        res.json(contact)
     })
 }
--- src/routes/subEmail.ts
+++ src/routes/subEmail.ts
@@ -1,23 +1,22 @@
 import { Request, Response } from "express"
+
+import { check, validationResult } from "express-validator"
 import { Emails } from "../models"
 import { _emails } from "../models/emails.model"
 
 export default function (app: any, path: any) {
     console.log("Loaded route: " + path)
     app.post(path, async (req: Request, res: Response) => {
-        const { email }: _emails = req.body
-        if (!email) return res.send({ message: "Invalid Request" })
-        const timestamp = Date.now()
-        Emails.create({
-            email,
+        await check("email", "Please enter a valid email").isEmail().run(req)
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        const email = new Emails({
+            email: req.body.email,
             subscribed: true,
-            timestamp: timestamp,
         })
-            .then(() => {})
-            .catch((e) => {
-                console.log(e)
-            })
-
-        return res.json({})
+        await email.save()
+        res.json(email)
     })
 }
